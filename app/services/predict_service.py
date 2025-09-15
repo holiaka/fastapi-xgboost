21ff519ae9bab2242ccab6137eb20c4c46a99124
Dmytrii Holiaka <holyaka_d_m@ukr.net>
2025-09-15 06:10:20 +0300

    added to selector of optimal values

--- app/services/predict_service.py
+++ app/services/predict_service.py
@@ -23,6 +23,41 @@ class PredictService:
                 "unit": "t/ha",
                 "label": "Trunk bark biomass",
             },
+            "branch": {
+                "file": "04_M_branch.json",
+                "unit": "t/ha",
+                "label": "Branch biomass",
+            },
+            "foliage": {
+                "file": "05_M_foliage.json",
+                "unit": "t/ha",
+                "label": "Foliage biomass",
+            },
+            "lg-growing-stock": {
+                "file": "01_LN_GS.json",
+                "unit": "m³/ha",
+                "label": "Growing stock generated on the log scale and back-transformed to the original scale",
+            },
+            "lg-trunk": {
+                "file": "02_LN_M_all_stem.json",
+                "unit": "t/ha",
+                "label": "Total trunk biomass generated on the log scale and back-transformed to the original scale",
+            },
+            "lg-trunk-bark": {
+                "file": "03_LN_M_stem_bark.json",
+                "unit": "t/ha",
+                "label": "Trunk bark biomass generated on the log scale and back-transformed to the original scale",
+            },
+            "lg-branch": {
+                "file": "04_LN_M_branch.json",
+                "unit": "t/ha",
+                "label": "Branch biomass generated on the log scale and back-transformed to the original scale",
+            },
+            "lg-foliage": {
+                "file": "05_LN_M_foliage.json",
+                "unit": "t/ha",
+                "label": "Foliage biomass generated on the log scale and back-transformed to the original scale",
+            },
         }
         self.models = self.load_models()
 
@@ -47,13 +82,16 @@ class PredictService:
 
         for key, model in self.models.items():
             prediction = model.predict(features)
-            mean = float(prediction[0])
-            ci_lower, ci_upper = self.estimate_confidence_interval(mean)
-            results[key] = {
+            print(f"Model: {key}, Prediction: {prediction}")
+            output = float(prediction[0])
+            if "lg-" in key:
+                output = np.exp(output)  # зворотне перетворення логарифму
+            ci_lower, ci_upper = self.estimate_confidence_interval(output)
+            results.all[key] = {
                 "label": self.model_info[key]["label"],
-                "value": f"{mean:.2f}",
+                "value": f"{output:.0f}",
                 "unit": self.model_info[key]["unit"],
-                "ci_95": f"{ci_lower:.2f} – {ci_upper:.2f}",
+                "ci_95": f"{ci_lower:.0f} – {ci_upper:.0f}",
             }
         return results
 
